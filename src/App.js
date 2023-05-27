0cad62e21907b5286b6fb4beb2243297f9e48099
ankit-3 <guptaankit61322@gmail.com>
2023-05-27 01:47:19 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+ const DarkModeToggle = () => {
+  const [isDark, setIsDark] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setIsDark(!isDark)}>
+        Toggle dark mode
+      </button>
+
+      {isDark && (
+        <>
+          {/* Change the background color here */}
+          <style jsx global>{`
+            :root {
+              --bg-color: #333; /* Black */
+            }
+
+            html {
+              background-color: var(--bg-color);
+            }
+          `}</style>
+        </>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+ export default DarkModeToggle;
